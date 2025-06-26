f4964029da99a7a5db04b4604197afed0b47bbf1
MordeKalver <silverwolf5665@gmail.com>
2025-06-26 11:54:56 +0200

    trollv1.ps1

--- troll.ps1
+++ troll.ps1
@@ -4,29 +4,51 @@ Invoke-WebRequest -Uri "https://people.com/thmb/IVbPa4OcPf0pthAT_LGEDiEyeYU=/150
 Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name Wallpaper -Value $img
 RUNDLL32.exe user32.dll,UpdatePerUserSystemParameters
 
-# 2 - Mostrar cadena de mensajes troll
+# 2 - Activar alto contraste
+Set-ItemProperty -Path "HKCU:\Control Panel\Accessibility\HighContrast" -Name "Flags" -Value "1"
+RUNDLL32.exe user32.dll,UpdatePerUserSystemParameters
+
+# 3 - Bajar velocidad del ratón
+Set-ItemProperty -Path "HKCU:\Control Panel\Mouse" -Name "MouseSensitivity" -Value 1
+RUNDLL32.exe user32.dll,UpdatePerUserSystemParameters
+
+# 4 - Invertir botones del ratón
+Set-ItemProperty -Path "HKCU:\Control Panel\Mouse" -Name "SwapMouseButtons" -Value 1
+RUNDLL32.exe user32.dll,UpdatePerUserSystemParameters
+
+# 5 - Mostrar mensajes troll
 Add-Type -AssemblyName PresentationFramework
 
 [System.Windows.MessageBox]::Show(
-  "Acepto que soy culpable de haber dejado el equipo desprotegido y todo es culpa mia",
-  "Confesion",[System.Windows.MessageBoxButton]::OK,
+  "Se ha detectado una negligencia de seguridad grave.\n\nHas dejado el equipo desbloqueado.",
+  "Informe del Comité Disciplinario",
+  [System.Windows.MessageBoxButton]::OK,
   [System.Windows.MessageBoxImage]::Error)
 
-$r = [System.Windows.MessageBox]::Show(
-  "Prometo no dejar el equipo desbloqueado otra vez",
-  "Promesa",[System.Windows.MessageBoxButton]::YesNo,
-  [System.Windows.MessageBoxImage]::Question)
+$response = [System.Windows.MessageBox]::Show(
+  "¿Prometes bloquear tu equipo cada vez que te ausentes?",
+  "Juramento de Seguridad",
+  [System.Windows.MessageBoxButton]::YesNo,
+  [System.Windows.MessageBoxImage]::Warning)
 
-if ($r -eq [System.Windows.MessageBoxResult]::Yes) {
+if ($response -eq [System.Windows.MessageBoxResult]::Yes) {
     [System.Windows.MessageBox]::Show(
-      "Buen chico, que no vuelva a ocurrir",
-      "OK",[System.Windows.MessageBoxButton]::OK,
+      "Buen chico. Que no vuelva a pasar.",
+      "Aprobado",
+      [System.Windows.MessageBoxButton]::OK,
       [System.Windows.MessageBoxImage]::Information)
 } else {
-    1..100 | ForEach-Object {
+    1..50 | ForEach-Object {
+        Start-Sleep -Milliseconds 150
         [System.Windows.MessageBox]::Show(
-          "PERO COMO QUE NO CABESTRO???",
-          "Error",[System.Windows.MessageBoxButton]::OK,
-          [System.Windows.MessageBoxImage]::Error)
+          "¿¡CÓMO QUE NO, CABESTRO?!",
+          "ERROR $_",
+          [System.Windows.MessageBoxButton]::OK,
+          [System.Windows.MessageBoxImage]::Hand)
     }
 }
+
+# 6 - Voz burla (opcional)
+Add-Type -AssemblyName System.Speech
+$voz = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$voz.Speak("Tu equipo fue invadido. Bloquéalo la próxima vez.")
